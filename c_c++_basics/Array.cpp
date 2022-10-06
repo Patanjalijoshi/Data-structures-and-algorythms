d5cd840ad83bd15a995a57550edc6e7f84d8d763
Patanjali <joshipattu12@gmail.com>
2022-10-06 17:24:40 +0530

    Create Array.cpp

---
+++ c_c++_basics/Array.cpp
@@ -0,0 +1,13 @@
+// this program is about understanding the basics of arrays
+
+#include<iostream>
+using namespace std;
+int main(){
+
+   int a[5] = {1,2,3,4,5};
+
+   for(int i =0; i<5; i++){
+    cout << a[i] << endl;
+   }  
+
+}
