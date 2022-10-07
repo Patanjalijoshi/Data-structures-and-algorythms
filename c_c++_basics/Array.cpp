360591510e5aa95f9aba28b28c961ad52e56565a
Patanjali <joshipattu12@gmail.com>
2022-10-07 05:55:46 +0530

    Update Array.cpp

--- c_c++_basics/Array.cpp
+++ c_c++_basics/Array.cpp
@@ -1,13 +1,22 @@
 // this program is about understanding the basics of arrays
-
+//take input from user and fill up an array and display usingfor each loop 
 #include<iostream>
 using namespace std;
-int main(){
-
-   int a[5] = {1,2,3,4,5};
-
-   for(int i =0; i<5; i++){
-    cout << a[i] << endl;
-   }  
-
+int main()
+{  
+    int element;
+    //int n;
+    int A[5]; 
+    //cout<<"Enter the number of elements:"<<endl;
+    //cin>>n;
+    for(int i = 0; i<=5; i++){
+        cout<<"Enter an integer element"<<endl;
+        cin>>element;
+        A[i]=element;
+    }
+    // using the for each loop to print the array 
+    for(int x : A)
+    cout<<x<<" ";
+    
+    return 0;
 }
