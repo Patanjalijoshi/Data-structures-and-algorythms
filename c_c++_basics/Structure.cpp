2e687076e362eb897f25cc3d0b630b82cf8eb4ea
Patanjali <joshipattu12@gmail.com>
2022-10-07 06:13:59 +0530

    structure

---
+++ c_c++_basics/Structure.cpp
@@ -0,0 +1,18 @@
+//defining a structure and doing some basic operations
+
+#include<iostream>
+using namespace std;
+
+struct rectangle
+{
+  float length;
+  float bredth;
+};
+int main()
+{
+    struct rectangle r;
+    r = {3.3,4.4};
+    cout<<"area of the rectangle = "<<r.bredth*r.length;
+
+    return 0;
+}
