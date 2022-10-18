6feb43911105276d21bda44a7afa3fcf84a7d15c
Patanjali <joshipattu12@gmail.com>
2022-10-18 22:17:18 +0530

    indirect recursion

---
+++ Recursion/Tail_head.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+//Tail recursion
+// all the operations are performed at calling time
+// void fun(int n)
+// {
+//     if(n>0)
+//     {
+//         cout<<n<<endl;
+//         fun(n-1);
+//     }
+// }
+
+//head recurssion
+// no operations are performed at the time of function call
+// all operations are performed at return time
+void fun(int n)
+{
+    if(n>0)
+    {
+        
+        fun(n-1);
+        cout<<n<<endl;
+    }
+}
+int main()
+{   
+    int x =3;
+    fun(x);
+    return 0;
+}
